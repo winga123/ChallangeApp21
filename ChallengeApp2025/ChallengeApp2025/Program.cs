068098fba54f483862a591b7d7adc41c80905d47
Sylwia Rumińska <sbwinga@gmail.com>
2025-11-29 07:52:10 +0100

    day1

--- ChallengeApp2025/ChallengeApp2025/Program.cs
+++ ChallengeApp2025/ChallengeApp2025/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var myName = "Sylwia";
+var mySex = "women";
+var myAge = 37;
+
+if (myAge < 30)
+{
+    if (mySex == "women")
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+}
+
+if (myAge == 30)
+{
+    if (myName == "Ewa")
+    {
+        Console.WriteLine("Ewa, lat 30");
+    }
+}
+
+if (mySex == "men")
+{
+    if (myAge < 18)
+    {
+        Console.WriteLine("Niepełnoletni mężczyzna");
+    }
+}
